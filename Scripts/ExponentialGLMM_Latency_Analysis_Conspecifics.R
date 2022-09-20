758a49fe3e733923b2b68dd53046610cd657990d
SarahBassing <sarah.bassing@gmail.com>
2022-09-20 16:48:56 -0700

    Function to bundle data, source JAGS models

--- Scripts/ExponentialGLMM_Latency_Analysis_Conspecifics.R
+++ Scripts/ExponentialGLMM_Latency_Analysis_Conspecifics.R
@@ -21,7 +21,7 @@
   library(tidyverse)
   
   #'  Read in data
-  load("./Outputs/tbd_conspif_2022-09-20.RData") #2022-09-07 2022-09-13
+  load("./Outputs/tbd_conspif_2022-09-20.RData") 
   #'  Remove observations involving lynx due to too few lynx detections
   tbd_conspif <- tbd_conspif %>%
     #'  Change units of time
@@ -32,10 +32,10 @@
     dplyr::select(-TimeSinceLastDet)
   
   #'  Filter images by ungulate species
-  tbd_md <- filter(tbd_conspif, Species == "Mule Deer")
-  tbd_elk <- filter(tbd_conspif, Species == "Elk")
-  tbd_moose <- filter(tbd_conspif, Species == "Moose")
-  tbd_wtd <- filter(tbd_conspif, Species == "White-tailed Deer")
+  tbd_md_con <- filter(tbd_conspif, Species == "Mule Deer")
+  tbd_elk_con <- filter(tbd_conspif, Species == "Elk")
+  tbd_moose_con <- filter(tbd_conspif, Species == "Moose")
+  tbd_wtd_con <- filter(tbd_conspif, Species == "White-tailed Deer")
   
   #'  Function to identify time cutoff between detections
   tbd_summary <- function(tbd, spp, quant) {
@@ -66,133 +66,164 @@
     #'  Return dataset after removing extreme values
     return(short_tbd)
   }
-  tbd_md_short <- tbd_summary(tbd_md, spp = "mule deer", quant = 0.97)
-  tbd_elk_short <- tbd_summary(tbd_elk, spp = "elk", quant = 0.97)
-  tbd_moose_short <- tbd_summary(tbd_moose, spp = "moose", quant = 0.95)
-  tbd_wtd_short <- tbd_summary(tbd_wtd, spp = "white-tailed deer", quant = 0.97)
+  tbd_md_con_short <- tbd_summary(tbd_md_con, spp = "mule deer", quant = 0.97)
+  tbd_elk_con_short <- tbd_summary(tbd_elk_con, spp = "elk", quant = 0.97)
+  tbd_moose_con_short <- tbd_summary(tbd_moose_con, spp = "moose", quant = 0.95)
+  tbd_wtd_con_short <- tbd_summary(tbd_wtd_con, spp = "white-tailed deer", quant = 0.97)
   
-  tbd_all_short <- rbind(tbd_md_short, tbd_elk_short, tbd_moose_short, tbd_wtd_short)
+  tbd_all_con_short <- rbind(tbd_md_con_short, tbd_elk_con_short, tbd_moose_con_short, tbd_wtd_con_short)
   
-  
-  #####  Set up model in BUGS language  ####
-  #'  -----------------------------------
-  cat(file = './Outputs/TimeBtwnDetections/tbd_conspecific.txt', "
-        model{
-        
-        #'  Define priors
-        #'  -------------
-        #'  Prior for intercept
-        alpha0 ~ dnorm(0, 0.001)
-        
-        #'  Priors for categorical beta coefficients
-        #'  Season
-        beta1[1] <- 0
-        for(hh in 2:4){
-          beta1[hh] ~ dnorm(0, 0.01)  # dunif(-10,10)
-        }
-        
-        #'  Prior for random effect for each camera location
-        for(j in 1:ncams){
-          alpha[j] ~ dnorm(0, tau.alpha) # mu.alpha for mean if no intercept (alpha0)
-        } 
-        
-        #'  Priors for TRI and PercForest
-        for(k in 1:2){  #ncovs
-          beta[k] ~ dnorm(0, 0.0001)
-        }
-        
-        #'  Hyperpriors for random effect
-        #'  -----------------------------
-        #'  Precision = 1/variance
-        sigma ~ dunif(0, 10)  
-        tau.alpha <- pow(sigma, -2) 
-        
-        #'  Define likelihood
-        #'  -----------------
-        for(i in 1:ntbd){
-          y[i] ~ dexp(lambda[i])   
-  
-          lambda[i] <- 1/mu[i]
-        
-          log(mu[i]) <- alpha0 + beta[1]*covs[i, 2] + beta[2]*covs[i, 3] + beta1[covs[i,1]] + alpha[site[i]]
-        }
-        
-        #'  Derived parameters
-        #'  ------------------
-        #'  mu.mu = mean number of minutes between events
-        mu.mu <- mean(mu[])
-    
-        }
-        ")
-  
-  #####  Define and bundle data for JAGS  ####
-  #'  -------------------------------------
-  #'  Filter to a single ungulate species
-  tbd_pp <- tbd_md_short
-  #'  Number of time-btwn-detection observations
-  ntbd <- nrow(tbd_pp)
-  #'  Number of unique camera locations
-  ncams <- length(unique(tbd_pp$CameraLocation))
-  #'  Format covariate data
-  tbd_dat <- dplyr::select(tbd_pp, c(tbd_min, tbd_hour, tbd_day, CameraLocation, 
-                                     Season, Complexity_index1, TRI, TRI_250m,
-                                     PercForest, Species)) %>%
-    mutate(cams = as.numeric(factor(CameraLocation), levels = CameraLocation), # must be 1 - 313 (not 0 - 312) if using nested indexing for random effect
-           Season = as.numeric(factor(Season, levels = c("Summer", "Fall", "Winter", "Spring"))), # levels must be 1-4 (not 0-3) for nested indexing
-           Complexity_index1 = scale(Complexity_index1),
-           TRI = scale(TRI),
-           PercForest = scale(PercForest))
-  
-  summary(tbd_dat)
-  head(tbd_dat)
-  #'  Covariate matrix for JAGS
-  covs <- matrix(NA, ncol = 4, nrow = ntbd)
-  covs[,1] <- tbd_dat$Season
-  covs[,2] <- tbd_dat$TRI
-  covs[,3] <- tbd_dat$PercForest
-  covs[,4] <- tbd_dat$Complexity_index1
-  head(covs)
-  #'  Number of covariates
-  ncovs <- ncol(covs)
-  
-  #'  Time-between-detections
-  tbd <- tbd_pp$tbd_min
-  # tbd <- tbd_pp$tbd_hour
-  # tbd <- tbd_pp$tbd_day
-  summary(tbd)
-  hist(tbd)
-  
-  bundled <- list(y = tbd, covs = covs, ncams = ncams, ncovs = ncovs, ntbd = ntbd,
-                  site = tbd_dat$cams)
-  
-  #####  Initial values, monitor parameters and specify MCMC settings  ####
-  #'  -----------------------------------------------------------------
-  #'  Set up initial values
-  alpha.init <- log(aggregate(tbd, list(tbd_dat$cams), FUN = mean)[,2])
-  inits <- function(){list(alpha = alpha.init, beta = runif(2,-1,1))}  #beta = runif(ncovs,-1,1)
-  #why does it not need inits for alpha0, beta1, beta2, etc.???
-  
-  #'  Parameters to be monitored
-  params <- c("mu.mu", "alpha0", "beta", "beta1", "sigma") # 
-  
+  ####  Setup data & MCMC specifications for JAGS  ####
+  #'  ----------------------------------------------
   #'  MCMC settings
   nc <- 3; ni <- 50000; nb <- 10000; nt <- 5; na <- 2000
   
-  #'  Run model in JAGS
-  #'  -----------------
+  #'  Function to define and bundle data
+  bundle_dat <- function(dat) {
+    #'  Number of time-btwn-detection observations
+    ntbd <- nrow(dat)
+    #'  Number of unique camera locations
+    ncams <- length(unique(dat$CameraLocation))
+    #'  Format covariate data
+    tbd_dat <- dplyr::select(dat, c(tbd_min, tbd_hour, tbd_day, CameraLocation, 
+                                    Season, Complexity_index1, TRI, TRI_250m,
+                                    PercForest, Species)) %>%
+      mutate(cams = as.numeric(factor(CameraLocation), levels = CameraLocation), # must be 1 - 313 (not 0 - 312) if using nested indexing for random effect
+             Season = as.numeric(factor(Season, levels = c("Summer", "Fall", "Winter", "Spring"))), # levels must be 1-4 (not 0-3) for nested indexing
+             Complexity_index1 = scale(Complexity_index1),
+             TRI = scale(TRI),
+             PercForest = scale(PercForest))
+    
+    print(summary(tbd_dat))
+    print(head(tbd_dat))
+    
+    #'  Covariate matrix for JAGS
+    covs <- matrix(NA, ncol = 4, nrow = ntbd)
+    covs[,1] <- tbd_dat$Season
+    covs[,2] <- tbd_dat$TRI
+    covs[,3] <- tbd_dat$PercForest
+    covs[,4] <- tbd_dat$Complexity_index1
+    print(head(covs))
+    
+    #'  Number of covariates
+    ncovs <- ncol(covs)
+    
+    #'  Time-between-detections
+    tbd <- dat$tbd_min
+    print(summary(tbd))
+    hist(tbd)
+    
+    bundled <- list(y = tbd, covs = covs, ncams = ncams, ncovs = ncovs, ntbd = ntbd,
+                    site = tbd_dat$cams)
+    return(bundled)
+  }
+  md_con_bundled <- bundle_dat(tbd_md_con_short)
+  elk_con_bundled <- bundle_dat(tbd_elk_con_short)
+  moose_con_bundled <- bundle_dat(tbd_moose_con_short)
+  wtd_con_bundled <- bundle_dat(tbd_wtd_con_short)
+  
+  
+  #'  ------------------------
+  #####  MULE DEER Analysis  ####
+  #'  ------------------------
+  #'  Source JAGS model
+  #'  Make sure inits and parameters being monitored match up with sourced model
+  #'  Make sure model parameterization matches order of covariates in bundled data
+  source("./Scripts/JAGS_models/JAGS_tbdconspecific_season_habitat.R")
+  
+  #'  Set up initial values
+  alpha.init <- log(aggregate(md_con_bundled$y, list(md_con_bundled$site), FUN = mean)[,2])
+  inits <- function(){list(alpha = alpha.init, beta = runif(2,-1,1))} 
+  
+  #'  Parameters to be monitored
+  params <- c("alpha0", "beta", "beta1", "sigma", "mu.mu") #"beta3", "beta4", 
+  
+  #'  Run model
   start.time <- Sys.time()
-  tbd.mod <- jags(bundled, params, './Outputs/TimeBtwnDetections/tbd_conspecific.txt',
+  tbd.md <- jags(md_con_bundled, params, './Outputs/TimeBtwnDetections/tbd_season_habitat.txt',
+                      inits = inits, n.chains = nc, n.iter = ni, n.burnin = nb, n.thin = nt,
+                      n.adapt = na, parallel = TRUE)
+  end.time <- Sys.time(); (run.time <- end.time - start.time)
+  print(tbd.md)
+  mcmcplot(tbd.md$samples)
+  save(tbd.md, file = "./Outputs/TimeBtwnDetections/tbd.md-season_habitat.RData")
+  
+  
+  #'  ------------------
+  #####  ELK Analysis  ####
+  #'  ------------------
+  #'  Source JAGS model
+  source("./Scripts/JAGS_models/JAGS_tbdconspecific_season_habitat.R")
+  
+  #'  Set up initial values
+  alpha.init <- log(aggregate(elk_con_bundled$y, list(elk_con_bundled$site), FUN = mean)[,2])
+  inits <- function(){list(alpha = alpha.init, beta = runif(2,-1,1))} 
+  
+  #'  Parameters to be monitored
+  params <- c("alpha0", "beta", "beta1", "sigma", "mu.mu") #"beta3", "beta4", 
+  
+  #'  Run model
+  start.time <- Sys.time()
+  tbd.elk <- jags(elk_con_bundled, params, './Outputs/TimeBtwnDetections/tbd_season_habitat.txt',
+                 inits = inits, n.chains = nc, n.iter = ni, n.burnin = nb, n.thin = nt,
+                 n.adapt = na, parallel = TRUE)
+  end.time <- Sys.time(); (run.time <- end.time - start.time)
+  print(tbd.elk)
+  mcmcplot(tbd.elk$samples)
+  save(tbd.elk, file = "./Outputs/TimeBtwnDetections/tbd.elk-season_habitat.RData")
+  
+  
+  #'  --------------------
+  #####  MOOSE Analysis  ####
+  #'  --------------------
+  #'  Source JAGS model
+  source("./Scripts/JAGS_models/JAGS_tbdconspecific_season_habitat.R")
+  
+  #'  Set up initial values
+  alpha.init <- log(aggregate(moose_con_bundled$y, list(moose_con_bundled$site), FUN = mean)[,2])
+  inits <- function(){list(alpha = alpha.init, beta = runif(2,-1,1))} 
+  
+  #'  Parameters to be monitored
+  params <- c("alpha0", "beta", "beta1", "sigma", "mu.mu") #"beta3", "beta4", 
+  
+  #'  Run model
+  start.time <- Sys.time()
+  tbd.moose <- jags(moose_con_bundled, params, './Outputs/TimeBtwnDetections/tbd_season_habitat.txt',
                   inits = inits, n.chains = nc, n.iter = ni, n.burnin = nb, n.thin = nt,
                   n.adapt = na, parallel = TRUE)
   end.time <- Sys.time(); (run.time <- end.time - start.time)
-  print(tbd.mod)
-  mcmcplot(tbd.mod$samples)
-  # tbd.mod$mean
-  # tbd.mod$summary
-  # which(tbd.mod$summary[,"Rhat"] > 1.1)
-  # save(tbd.mod, file="./Outputs/TimeBtwnDetections/tbd_global.Rdata")
+  print(tbd.moose)
+  mcmcplot(tbd.moose$samples)
+  save(tbd.moose, file = "./Outputs/TimeBtwnDetections/tbd.moose-season_habitat.RData")
+  
+  
+  #'  --------------------------------
+  #####  WHITE-TAILED DEER Analysis  ####
+  #'  --------------------------------
+  #'  Source JAGS model
+  #'  Make sure inits and parameters being monitored match up with sourced model
+  #'  Make sure model parameterization matches order of covariates in bundled data
+  source("./Scripts/JAGS_models/JAGS_tbdconspecific_season_habitat.R")
+  
+  #'  Set up initial values
+  alpha.init <- log(aggregate(wtd_con_bundled$y, list(wtd_con_bundled$site), FUN = mean)[,2])
+  inits <- function(){list(alpha = alpha.init, beta = runif(2,-1,1))} 
+  
+  #'  Parameters to be monitored
+  params <- c("alpha0", "beta", "beta1", "sigma", "mu.mu") #"beta3", "beta4", 
+  
+  #'  Run model
+  start.time <- Sys.time()
+  tbd.wtd <- jags(wtd_con_bundled, params, './Outputs/TimeBtwnDetections/tbd_season_habitat.txt',
+                 inits = inits, n.chains = nc, n.iter = ni, n.burnin = nb, n.thin = nt,
+                 n.adapt = na, parallel = TRUE)
+  end.time <- Sys.time(); (run.time <- end.time - start.time)
+  print(tbd.wtd)
+  mcmcplot(tbd.wtd$samples)
+  save(tbd.wtd, file = "./Outputs/TimeBtwnDetections/tbd.wtd-season_habitat.RData")
   
   
   
+ 
   ####  EVENTUALLY DO SOME ASSESSMENT OF GOODNESS OF FIT  ####
   
