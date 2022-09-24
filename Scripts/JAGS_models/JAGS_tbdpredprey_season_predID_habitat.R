70081c2ae7dee0e980b15489b233eb6d7db3c2ba
SarahBassing <sarah.bassing@gmail.com>
2022-09-23 22:33:29 -0700

    fix derived params

--- Scripts/JAGS_models/JAGS_tbdpredprey_season_predID_habitat.R
+++ Scripts/JAGS_models/JAGS_tbdpredprey_season_predID_habitat.R
@@ -39,17 +39,6 @@
               beta2[jj] ~ dnorm(0, 0.01)  
             }
             
-            #' #'  Interaction for TRI * predator species ID 
-            #' beta3[1] <- 0
-            #' for(jj in 2:5){
-            #'   beta3[jj] ~ dnorm(0, 0.01)
-            #' }
-            #' 
-            #' #'  Interaction for PercForest * predator species ID 
-            #' beta4[1] <- 0
-            #' for(jj in 2:5){
-            #'   beta4[jj] ~ dnorm(0, 0.01)
-            #' }
             
             #'  Prior for random effect for each camera location
             for(j in 1:ncams){
@@ -70,15 +59,36 @@
       
               lambda[i] <- 1/mu[i]
             
-              log(mu[i]) <- alpha0 + beta1[covs[i,1]] + beta2[covs[i,2]] + 
-                            beta[1]*covs[i, 4] + beta[2]*covs[i, 5] + 
-                            # beta3[covs[i,2]]*covs[i, 4] + beta4[covs[i,2]]*covs[i, 5] + 
-                            alpha[site[i]]
+              log(mu[i]) <- alpha0 + beta1[covs[i,1]] + beta2[covs[i,2]] + #beta3[covs[i,6]] +
+                            beta[1]*covs[i,4] + beta[2]*covs[i,5] + alpha[site[i]]
             }
             
             #'  Derived parameters
             #'  ------------------
-            #'  Mean tbd per month, predator, and camera site
+            #'  Mean tbd per season & predator at mean TRI & PercForest
+            for(hh in 1:4){
+              for(jj in 1:5){
+                tbd[hh, jj] <- exp(alpha0 + beta1[hh] + beta2[jj] + beta[1]*0 + beta[2]*0)
+              }
+            } 
+             
+      
+            #'  Mean tbd per season 
+            for(hh in 1:4){
+              season.tbd[hh] <- mean(tbd[hh,])
+            }
+      
+            #'  Mean tbd per predator 
+            for(jj in 1:5){
+              pred.tbd[jj] <- mean(tbd[,jj])
+            }
+      
+            #' Mean number of minutes between events
+            mu.tbd <- mean(tbd[,])
+            mu.mu <- mean(mu[])
+      
+      
+            # #'  Mean tbd per month, predator, and camera site
             # for(j in 1:ncams){
             #   for(hh in 1:4){
             #     for(jj in 1:5){
@@ -87,44 +97,23 @@
             #     }
             #   }
             # }
-      #'  Mean tbd per month, predator, and camera site
-            for(hh in 1:4){
-              for(jj in 1:5){
-                tbd[hh, jj] <- exp(alpha0 + beta1[hh] + beta2[jj] +
-                beta[1]*0 + beta[2]*0)
-              }
-            }
-
             #' #'  Mean tbd per month and predator
             #' for(hh in 1:4){
             #'   for(jj in 1:5){
             #'     mean.tbd[hh, jj] <- mean(tbd[, hh, jj])
             #'   }
             #' }
-            #' #'  Mean tbd per season assuming all predators are equal sample size
+            #' #'  Mean tbd per season assumes all predators are equal sample size
             #' for(hh in 1:4){
             #'   season.tbd[hh] <- mean(mean.tbd[hh,])
             #' }
-      
-      #'  Mean tbd per season accounts for sample size in all the seasons
-            for(hh in 1:4){
-              season.tbd[hh] <- mean(tbd[hh,])
-            }
-      
-            #' #'  Mean tbd per predator assuming all seasons are equal sample size
+            #' #'  Mean tbd per predator assumes all seasons are equal sample size
             #' for(jj in 1:5){
             #'   pred.tbd[jj] <- mean(mean.tbd[,jj])
             #' }
-      
-      #'  Mean tbd per predator accounts for sample size in all the predators
-            for(jj in 1:5){
-              pred.tbd[jj] <- mean(tbd[,jj])
-            }
             #' #' Mean number of minutes between events across any season - currently skewed by whichever season has larger smaple size
             #' mu.tbd <- mean(season.tbd[])
-      
-      mu.tbd <- mean(tbd[,])
-      mu.mu <- mean(mu[])
         
             }
             ")
+  
